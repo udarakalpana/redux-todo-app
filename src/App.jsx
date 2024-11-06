e228c70e357b28b7e268e90543226df51b002afd
kalpana udara <kalpanaudara058@gmail.com>
2024-11-06 19:55:28 +0530

    Create UI for add task app.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ const App = () => {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <div className="m-4 grid grid-cols-2">
+              <div>
+                  <label htmlFor="default-input"
+                         className="mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Task</label>
+                  <input type="text" id="default-input"
+                         className="input_field"/>
+              </div>
+              <div className='mt-4'>
+                  <button type="button"
+                          className="dark_btn m-2">Add New Task
+                  </button>
+              </div>
+          </div>
       </>
   )
 }
