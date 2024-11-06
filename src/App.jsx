db42ef1be2eac3b49e445b5610bfc197bd7faab6
kalpana udara <kalpanaudara058@gmail.com>
2024-11-06 19:59:45 +0530

    Installed prettier for code format.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 const App = () => {
-
   return (
-      <>
-          <div className="m-4 grid grid-cols-2">
-              <div>
-                  <label htmlFor="default-input"
-                         className="mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Task</label>
-                  <input type="text" id="default-input"
-                         className="input_field"/>
-              </div>
-              <div className='mt-4'>
-                  <button type="button"
-                          className="dark_btn m-2">Add New Task
-                  </button>
-              </div>
-          </div>
-      </>
-  )
-}
+    <>
+      <div className="m-4 grid grid-cols-2">
+        <div>
+          <label
+            htmlFor="default-input"
+            className="mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Task
+          </label>
+          <input type="text" id="default-input" className="input_field" />
+        </div>
+        <div className="mt-4">
+          <button type="button" className="dark_btn m-2">
+            Add New Task
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
